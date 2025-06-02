1732e05eb385b5cb45765e8a197f5c067428a255
Anxhoel Nallbani <anxhoelnallbani@gmail.com>
2025-06-02 16:34:42 +0200

    kickstart

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,25 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user # Assuming Devise is used
+    if @flat.save
+      redirect_to flats_path, notice: "Flat created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :description, :location, :price_per_night)
+  end
+end
