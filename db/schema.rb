f6b6800e722b91d59e1889c3b59e2c92b5d340bb
Anxhoel Nallbani <anxhoelnallbani@gmail.com>
2025-06-04 12:38:40 +0200

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2025_06_03_151602) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "bookings", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "flat_id", null: false
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["flat_id"], name: "index_bookings_on_flat_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-=======
 ActiveRecord::Schema[7.1].define(version: 2025_06_03_151641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,9 +78,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_151641) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "bookings", "flats"
-  add_foreign_key "bookings", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "flats"
