e5e5f614385a62dd6b2679f83a26ca8082a1d5b3
Dan Bayliss <dan5083@outlook.com>
2025-06-04 11:19:22 +0100

    "tiny change"

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,12 +1,14 @@
-belongs_to :user  # this is the guest!
-belongs_to :flat
+class Booking < ApplicationRecord
+  belongs_to :user   # this is the guest
+  belongs_to :flat
 
-validates :status, inclusion: { in: %w[pending accepted declined], allow_blank: true }
-validate :end_date_after_start_date
+  validates :status, inclusion: { in: %w[pending accepted declined], allow_blank: true }
+  validate :end_date_after_start_date
 
-private
+  private
 
-def end_date_after_start_date
-  return if start_date.blank? || end_date.blank?
-  errors.add(:end_date, "must be after the start date") if end_date <= start_date
+  def end_date_after_start_date
+    return if start_date.blank? || end_date.blank?
+    errors.add(:end_date, "must be after the start date") if end_date <= start_date
+  end
 end
