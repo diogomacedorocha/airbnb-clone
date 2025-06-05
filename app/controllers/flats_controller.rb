549b65e3c1913976e4d481d7584a1d8f3cb8576d
Anxhoel Nallbani <anxhoelnallbani@gmail.com>
2025-06-05 12:33:22 +0200

    mapbox fix

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,22 @@
 class FlatsController < ApplicationController
   def index
     @flats = Flat.all
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def show
     @flat = Flat.find(params[:id])
+    @markers = [{
+      lat: @flat.latitude,
+      lng: @flat.longitude
+    }]
   end
+
 
   def new
     @flat = Flat.new
